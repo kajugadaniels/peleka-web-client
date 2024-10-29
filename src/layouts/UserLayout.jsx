204b41cf30f4d7afbbfd583f59c35d028a3efaf0
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 12:56:21 +0200

    New changes

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -9,7 +9,7 @@ const UserLayout = () => {
                 className="top-nav fixed top-0 left-0 w-full py-2 text-center duration-500 transition-all origin-top scale-y-100 bg-primary z-10"
                 id="top-nav"
             >
-                <p className="max-lg:text-sm text-paragraph dark:text-paragraph font-medium">
+                <p className="max-lg:text-sm text-paragraph dark:text-paragraph font-medium text-white">
                     Until recently, the prevailing view assumed
                 </p>
             </div>
