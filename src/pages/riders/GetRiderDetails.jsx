f4ad520eb966202a2e5048d374855230854898b3
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-01 13:51:21 +0200

    Retrieve driver details

--- src/pages/riders/GetRiderDetails.jsx
+++ src/pages/riders/GetRiderDetails.jsx
@@ -43,7 +43,52 @@ const GetRiderDetails = () => {
     }
 
     return (
-        <div>GetRiderDetails</div>
+        <div class="container mb-150 pt-[250px] max-md:pt-150 relative">
+            <div class="absolute left-1/2 top-[200px] w-full h-[550px] -translate-x-1/2 bg-cover  bg-[url('../images/hero-gradient.png')] bg-no-repeat bg-center opacity-70 md:hidden -z-10"></div>
+            <div class="grid grid-cols-2 max-md:grid-cols-1 gap-20 max-w-[800px] mx-auto relative auto-rows-min ">
+                <div class="absolute left-1/2 top-[37%] -translate-x-1/2 -translate-y-1/2 flex max-md:flex-col -z-10 max-md:hidden">
+                    <div
+                        class="max-1xl:w-[335px] max-1xl:h-[335px]  1xl:w-[442px] 1xl:h-[442px]  rounded-full bg-primary-200/30 blur-[145px]"
+                    ></div>
+                    <div
+                        class="max-1xl:w-[335px] max-1xl:h-[335px]  1xl:w-[442px] 1xl:h-[442px]  rounded-full bg-primary-200/50 -ml-[170px] max-md:ml-0 blur-[145px]"
+                    ></div>
+                    <div
+                        class="max-1xl:w-[335px] max-1xl:h-[335px]  1xl:w-[442px] 1xl:h-[442px]  rounded-full bg-primary-200/30 -ml-[170px] max-md:ml-0 blur-[145px]"
+                    ></div>
+                </div>
+                <div
+                    class="bg-white dark:bg-dark-200 rounded-medium p-2.5  shadow-box self-start"
+                    data-aos="fade-up"
+                    data-aos-offset="200"
+                    data-aos-duration="1000"
+                    data-aos-once="true"
+                >
+                    <div class=" rounded bg-gray-100 dark:bg-[#30302F]">
+                        <img
+                            src="https://aplio.vercel.app/images/team/teamSingle.png"
+                            alt={riderData.name}
+                            class="object-cover object-bottom"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <div class="mb-12">
+                        <p class="section-tagline">
+                            {riderData.code}
+                        </p>
+                        <h1 class="mb-12 text-[48px] font-bold">
+                            {riderData.name}
+                        </h1>
+                    </div>
+                    <div class="mb-12 flex items-center">
+                        <a href="tel:{rider.phone_number}">
+                            {riderData.phone_number}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
