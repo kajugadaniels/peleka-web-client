99c387bbd7eba98735f587582ac5d4ea4482e872
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-01 12:41:18 +0200

    Setup riders page

--- src/pages/riders/GetRiders.jsx
+++ src/pages/riders/GetRiders.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const GetRiders = () => {
     return (
-        <div>GetRiders</div>
+        <section className="hero  overflow-hidden relative max-lg:pt-150 pt-[240px] pb-[60px] z-40">
+            <div className="container">
+                <div
+                    className="max-w-[948px] mx-auto text-center"
+                    data-aos="fade-up"
+                    data-aos-offset="200"
+                    data-aos-duration="1000"
+                    data-aos-once="true"
+                >
+                    <p className="mb-4 font-medium uppercase">Our leading, strong and creative team</p>
+                    <h1 className="max-lg:mb-10 mb-10">Our Riders</h1>
+                    <p className="max-lg:mb-10 mb-12 max-w-[590px] mx-auto">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        
     )
 }
 
