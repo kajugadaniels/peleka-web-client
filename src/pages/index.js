74ce4bd4138ffa3294f6478a70768465fc0de8c7
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-04 12:13:16 +0200

    Setup login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+export { default as Login } from './auth/Login'
+
 export { default as Home } from './Home'
 export { default as About } from './About'
 export { default as Services } from './Services'
