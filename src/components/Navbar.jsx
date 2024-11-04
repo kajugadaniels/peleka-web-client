c78f861fc392de3c572afc4e8571fd13a6a19ac5
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-04 12:11:26 +0200

    New changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { LogoLight } from '../assets/img';
 import { Link, useLocation } from 'react-router-dom';
+import { Menu } from 'lucide-react'
 
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -121,30 +122,7 @@ const Navbar = () => {
                             className="outline-none mobile-menu-button w-10 h-10 rounded-full bg-white dark:bg-dark-200 relative"
                             onClick={toggleMobileMenu}
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="22"
-                                height="14"
-                                viewBox="0 0 22 14"
-                                fill="none"
-                                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-                            >
-                                <path
-                                    d="M0 1C0 0.447715 0.447715 0 1 0H21C21.5523 0 22 0.447715 22 1C22 1.55228 21.5523 2 21 2H1C0.447716 2 0 1.55228 0 1Z"
-                                    fill=""
-                                    className="fill-paragraph dark:fill-white"
-                                />
-                                <path
-                                    d="M8 7C8 6.44772 8.44772 6 9 6H21C21.5523 6 22 6.44772 22 7C22 7.55228 21.5523 8 21 8H9C8.44772 8 8 7.55228 8 7Z"
-                                    fill=""
-                                    className="fill-paragraph dark:fill-white"
-                                />
-                                <path
-                                    d="M4 13C4 12.4477 4.44772 12 5 12H21C21.5523 12 22 12.4477 22 13C22 13.5523 21.5523 14 21 14H5C4.44772 14 4 13.5523 4 13Z"
-                                    fill=""
-                                    className="fill-paragraph dark:fill-white"
-                                />
-                            </svg>
+                            <Menu className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
                         </button>
                     </li>
                 </ul>
